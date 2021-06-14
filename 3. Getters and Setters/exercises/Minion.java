0d4a9ef347f8528743383bee1315831f10fce507
Corbin Jensen <corbjens@gmail.com>
2021-06-14 15:21:03 -0700

    completed exercises in encapsalation / getters and setters

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -1,5 +1,59 @@
 package exercises;
 
 public class Minion {
+    private String name;
+    private int eyes;
+    private String color;
+    private String master;
 
+    // create a constructor
+    Minion(
+        String name, 
+        int eyes,
+        String color,
+        String master
+        ) 
+        {
+            this.name = name; 
+            this.eyes = eyes;
+            this.color = color;
+            this.master = master;
+        } // end constructor
+
+    // getters and setters for
+    // the member vars.
+
+    void getColor(){
+        System.out.println(this.color); 
+    }
+
+    void getName(){
+        System.out.println(this.name); 
+    }
+
+    void getEyes(){
+        System.out.println(this.eyes);
+    }
+
+    void getMaster() {
+        System.out.println(this.master); 
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int setEyes(int eyes) {
+        return this.eyes = eyes;
+    }
+
+    void setMaster(String master) {
+        this.master = master;
+    }
+
+    
 }
